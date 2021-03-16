8baabd96b9b177362e80e703fee79a216a2bf6fe
Mateusz Grabowski <m.grabowski@rolladengroup.de>
2021-03-16 17:19:45 +0100

    empty values module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { StationsModule } from './stations/stations.module';
 import { SensorsModule } from './sensors/sensors.module';
 import { IndexLevelModule } from './index-level/index-level.module';
+import { ValuesModule } from './values/values.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), StationsModule, SensorsModule, IndexLevelModule],
+  imports: [TypeOrmModule.forRoot(), StationsModule, SensorsModule, IndexLevelModule, ValuesModule],
   controllers: [AppController],
   providers: [AppService],
 })
